214f00c0ca6bce1b0b7e75d76ba1994b49f9e762
imostagi <imostagi>
2018-09-13 11:54:34 -0300

    Implementação recuperação de senha

--- index.php
+++ index.php
@@ -149,6 +149,79 @@ $app->get("/admin/users/:iduser", function($iduser){
 
  });
 
+ $app->get("/admin/forgot", function(){
+
+ 	$page = new PageAdmin([
+ 		"header"=>false,
+ 		"footer"=>false
+ 	]);
+
+ 	$page->setTpl("forgot");
+
+ });
+
+ $app->post("/admin/forgot", function(){
+
+ 	$user = User::getForgot($_POST["email"]);
+
+ 	header("Location: /admin/forgot/sent");
+
+ });
+
+ $app->get("/admin/forgot/sent", function(){
+
+ 	$page = new PageAdmin([
+ 		"header"=>false,
+ 		"footer"=>false
+ 	]);
+
+ 	$page->setTpl("forgot-sent");
+
+ });
+
+$app->post("/admin/forgot/reset", function(){
+
+	$user = User::validForgotDecrypt($_GET["code"]);
+
+	$page = new PageAdmin([
+ 		"header"=>false,
+ 		"footer"=>false
+ 	]);
+
+ 	$page->setTpl("forgot-reset", array(
+ 		"name"=>$user["desperson"],
+ 		"code"=>$_GET["code"]
+
+ 	));
+
+});
+
+$app->post("/admin/forgot/reset", function(){
+
+	$forgot = User::validForgotDecrypt($_POST["code"]);
+
+	User::setForgotUsed($forgot["idrecovery"]);
+
+	$user = new User();
+
+	$user->get((int)$forgot["iduser"]);
+
+	$password = password_hash($_POST["password"], PASSWORD_BCRYPT, [
+		"cost"=>12
+	]);
+
+	$user->setPassword($password);
+
+
+	$page = new PageAdmin([
+ 		"header"=>false,
+ 		"footer"=>false
+ 	]);
+
+ 	$page->setTpl("forgot-reset-success");
+
+});
+
 $app->run();
 
  ?>

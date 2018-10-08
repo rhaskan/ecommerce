15e31466549f4177f1b6e0c933121cb1931a070c
imostagi <imostagi>
2018-10-08 10:48:22 -0300

    ajuste endereço

--- views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Android</a></li><li><a href="/categories/4">Apple</a></li><li><a href="/categories/6">Motorola</a></li><li><a href="/categories/7">Samsung</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Android</a></li><li><a href="/categories/4">Apple</a></li><li><a href="/categories/8">Apple</a></li><li><a href="/categories/9">IOS</a></li><li><a href="/categories/6">Motorola</a></li><li><a href="/categories/7">Samsung</a></li>
